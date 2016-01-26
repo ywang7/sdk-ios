e6aa361bce0befc4524c72e727d57f56edcf948c
Yinghao Wang <yinghaowang@apples.local>
2016-01-26 12:44:48 -0800

    - updated project settings - added dynamic framework target - updated Unit tests

---
+++ sdk/AnetSDK/AnetSDK.h
@@ -0,0 +1,19 @@
+//
+//  AnetSDK.h
+//  AnetSDK
+//
+//  Created by Yinghao Wang on 1/26/16.
+//  Copyright © 2016 MMA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnetSDK.
+FOUNDATION_EXPORT double AnetSDKVersionNumber;
+
+//! Project version string for AnetSDK.
+FOUNDATION_EXPORT const unsigned char AnetSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnetSDK/PublicHeader.h>
+
+
